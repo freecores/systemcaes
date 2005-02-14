8eec097ddc5352077527e2e07c5790824c1d4ed0
jcastillo <jcastillo@7711d54f-b22a-4f04-94dc-5548449f87a6>
2005-02-14 11:00:16 +0000

    *** empty log message ***

--- bench/verilog/aes192lowarea/test_bench_top.v
+++ bench/verilog/aes192lowarea/test_bench_top.v
@@ -1,3 +1,45 @@
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+////  AES Test Bench                                             ////
+////                                                             ////
+////                                                             ////
+////  Author: Javier Castillo                                    ////
+////                                                             ////
+////                                                             ////
+////  Adapted to SystemC                                         ////
+////  AES project by: jcastillo@opensocdesign.com                ////
+////                                                             ////
+////                                                             ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+//// Copyright (C) 2000-2005 Javier Castillo                     ////
+////                                                             ////
+//// This source file may be used and distributed without        ////
+//// restriction provided that this copyright statement is not   ////
+//// removed from the file and that any derivative work contains ////
+//// the original copyright notice and the associated disclaimer.////
+////                                                             ////
+////     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ////
+//// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ////
+//// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ////
+//// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ////
+//// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ////
+//// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ////
+//// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ////
+//// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ////
+//// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ////
+//// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ////
+//// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ////
+//// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ////
+//// POSSIBILITY OF SUCH DAMAGE.                                 ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+//
+//  CVS Log
+//
+//      $Log: not supported by cvs2svn $
+
 `timescale 10ns/1ns
 
 module top;
@@ -31,7 +73,7 @@ aes192 d1 (clk,reset,load_i,decrypt_i,data_i,key_i,ready_o,data_o);
 	load_i = #17 'b1;
 	load_i = #22 'b0;
 	
-        $display("Ejecutando");
+        $display("Running");
         wait(ready_o);	
 	$display("%H",data_o);
 	$finish;
